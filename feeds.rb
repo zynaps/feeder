6e86223fb543971158392ba8602a4c4bee1c5ea3
Igor Vinokurov <zynaps@zynaps.ru>
2017-07-26 08:10:27 +0200

    filtered feed for rutor.is/kino added

--- feeds.rb
+++ feeds.rb
@@ -3,6 +3,36 @@ require 'rss'
 require 'nokogiri'
 require 'sinatra'
 
+get %r{/zynaps/rutor-filtered}, :provides => 'rss' do
+  title_re = %r{
+    (?<titles>.*)\s+
+    \((?<year>\d+)\)\s+
+    (?<label>[\w\s-]+)\s+от\s+
+    (?<team>.*)\s+\|\s+
+    (?<versions>.*)
+  }x
+
+  feed = RSS::Parser.parse(open("http://alt.rutor.info/rss.php?full=1"))
+
+  feed.items.delete_if do |item|
+    if meta = item.title.match(title_re)
+      titles = meta['titles'].split('/').map(&:strip)
+      year = meta['year'].to_i
+      label = meta['label']
+      versions = meta['versions'].split(/[, ]+/)
+      team = meta['team']
+
+      item.title = format("%d %s | %s %s | %s", year, titles.join(' / '), team, label, versions.join(','))
+
+      next
+    end
+
+    true
+  end
+
+  feed.to_s
+end
+
 get %r{/bogorad/(?<url>(mediagazer|techmeme)\.com/feed.xml)}, :provides => 'rss' do |url|
   feed = RSS::Parser.parse(open("http://#{url}"))
 
