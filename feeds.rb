d1063077543ebc9fad460f333c6cd1de727667aa
Igor Vinokurov <zynaps@zynaps.ru>
2017-07-26 19:25:46 +0000

    no similar items in rutor.is/kino

--- feeds.rb
+++ feeds.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'rss'
 require 'nokogiri'
+require 'redis'
 require 'sinatra'
 
 get %r{/zynaps/rutor-filtered}, :provides => 'rss' do
@@ -26,6 +27,14 @@ get %r{/zynaps/rutor-filtered}, :provides => 'rss' do
     team = meta['team']
 
     next if label =~ /(1080|720)p/
+
+    cache = Redis.new
+    cache_key = "feeds:rutor-filtered:seen:%s:%d:%s" % [titles.last, year, versions.join(',')]
+
+    next if team !~ /Scarabey/i and cache.get(cache_key)
+
+    cache.set(cache_key, 1)
+    cache.expire(cache_key, 60 * 60 * 24 * 3)
 
     item.title = format("%s (%d) %s %s | %s", titles.join(' / '), year, label, team, versions.join(','))
 
