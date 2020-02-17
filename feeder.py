1222fa79afa9e65a531412a06ccb183bf22ffcfe
Igor Vinokurov <zynaps@zynaps.ru>
2020-02-17 13:38:34 +0300

    simple exceptions handling

--- feeder.py
+++ feeder.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# TODO: exceptions handling
 # TODO: check if we have entry content
 
 import os
@@ -24,15 +23,18 @@ def generate_feed(feed):
     fg.updated(redis.get('%s:updated' % feed_base).decode())
 
     for entry in redis.lrange("%s:entries" % feed_base, 0, 49):
-        entry = json.loads(entry.decode())
+        try:
+            entry = json.loads(entry.decode())
 
-        fe = fg.add_entry()
+            fe = fg.add_entry()
 
-        fe.id(entry['id'])
-        fe.title(entry['title'])
-        fe.content(entry['content'])
-        fe.link(href = entry['link'])
-        fe.updated(entry['updated'])
+            fe.id(entry['id'])
+            fe.title(entry['title'])
+            fe.content(entry['content'])
+            fe.link(href = entry['link'])
+            fe.updated(entry['updated'])
+        except:
+            continue
 
     return fg.atom_str(pretty = True)
 
