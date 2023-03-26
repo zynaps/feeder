ee7dcdf20b15f065758878a2869e382dfb96d086
Igor Vinokurov <zynaps@zynaps.ru>
2023-03-26 15:10:19 +0400

    using current ruby best pratices

--- Gemfile
+++ Gemfile
@@ -1,13 +1,14 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'json'
-gem 'redis'
-gem 'redis-namespace'
-gem 'rss'
-gem 'puma'
-gem 'sinatra'
+gem "json"
+gem "redis"
+gem "redis-namespace"
+gem "rss"
+gem "puma"
+gem "sinatra"
 
 group :development do
-  gem 'amazing_print'
-  gem 'pry'
+  gem "amazing_print"
+  gem "pry"
+  gem "standard"
 end
--- feeder.rb
+++ feeder.rb
@@ -1,29 +1,29 @@
-require 'redis'
-require 'redis-namespace'
-require 'json'
-require 'rss'
-require 'sinatra'
+require "redis"
+require "redis-namespace"
+require "json"
+require "rss"
+require "sinatra"
 
-get %r{/(?<feed>\w+)}, provides: 'atom' do |feed|
+get %r{/(?<feed>\w+)}, provides: "atom" do |feed|
   redis = Redis::Namespace.new("feeder:#{feed}", redis: Redis.new)
 
-  halt(404) if not redis.exists?('entries')
+  halt(404) if !redis.exists?("entries")
 
-  atom = RSS::Maker.make('atom') do |maker|
+  atom = RSS::Maker.make("atom") do |maker|
     maker.channel.id = "http://feeder.zynaps.ru/#{feed}"
-    maker.channel.author = redis.get('author') || 'zynaps@zynaps.ru'
-    maker.channel.title = redis.get('title')
-    maker.channel.updated = redis.get('updated') || Time.now.to_datetime.rfc3339
+    maker.channel.author = redis.get("author") || "zynaps@zynaps.ru"
+    maker.channel.title = redis.get("title")
+    maker.channel.updated = redis.get("updated") || Time.now.to_datetime.rfc3339
 
-    redis.lrange('entries', 0, 49).each do |entry|
+    redis.lrange("entries", 0, 49).each do |entry|
       entry = JSON.parse(entry)
 
       maker.items.new_item do |item|
-        item.id = entry['id']
-        item.title = entry['title']
-        item.content.content = entry['content'] if entry['content']
-        item.link = entry['link']
-        item.updated = entry['updated']
+        item.id = entry["id"]
+        item.title = entry["title"]
+        item.content.content = entry["content"] if entry["content"]
+        item.link = entry["link"]
+        item.updated = entry["updated"]
       end
     end
   end
